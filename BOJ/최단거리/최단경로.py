22c89fa07ddcdc33c10aa6910a2be5ea32a7c17f
JunHyeongKim73 <rla5764v@gmail.com>
2022-12-28 14:57:50 +0900

    최단경로 베스트 코드 공부

--- BOJ/최단거리/최단경로.py
+++ BOJ/최단거리/최단경로.py
@@ -1,62 +1,36 @@
-from sys import stdin
-from queue import PriorityQueue
+import sys
+import heapq
+input = sys.stdin.readline
+INF = 100000000
 
-V, E = map(int, stdin.readline().strip().split())
-K = int(stdin.readline())
+V,e = map(int, input().split())
+start = int(input())
+data = [[] for _ in range(V+1)]
+for _ in range(e):
+    u, v, w = map(int, input().split())
+    data[u].append((v, w))
 
-adj = [[] for _ in range(V+1)] 
+distance = [INF]*(V+1)
 
-for _ in range(E):
-    u, v, w = map(int, stdin.readline().strip().split())
-    adj[u].append((v, w))
-
-INF = 20000000
-res = [INF for _ in range(V+1)]
-visited = [False for _ in range(V+1)]
-
+q = []
 # 다익스트라 알고리즘
-def solve(start):
-    res[start] = 0
-    pq = PriorityQueue()
-    pq.put((0, start))
-    
-    while not pq.empty():
+def dijkstra(start):
+    distance[start] = 0 
+    heapq.heappush(q,(0,start))
+    while q:
         # 가장 작은 dist를 가진 노드를 찾는다
-        _, v = pq.get()
-        if visited[v]:
-            continue
-
-        visited[v] = True
+        dist, now_node = heapq.heappop(q)
 
         # 노드의 간선으로 다른 노드까지의 dist를 업데이트 한다
-        for nv, nw in adj[v]:
-            res[nv] = min(res[nv], res[v] + nw)
-            pq.put((res[nv], nv))
+        for n_n, weight in data[now_node]:
+            cost = dist + weight
+            if cost < distance[n_n]:
+                distance[n_n] = cost
+                heapq.heappush(q,(cost,n_n))
 
-if __name__ == "__main__":
-    solve(K)
-    for i in range(1, V+1):
-        if res[i] == INF:
-            print('INF')
-        else:
-            print(res[i])
-
-'''
-5 6
-1
-5 1 1
-1 2 2
-1 3 3
-2 3 4
-2 4 5
-3 4 6
-
-5 6
-1
-5 1 1
-1 2 3
-1 3 5
-2 3 1
-2 4 5
-3 4 1
-'''
+dijkstra(start)
+for i in distance[1:]:
+    if i != INF:
+        print(i)
+    else:
+        print("INF")
--- reference.py
+++ reference.py
@@ -1,24 +1,30 @@
 # 입력
-# from sys import stdin
+# import sys
+# input = sys.stdin.readline
 # n = int(input())
 
 # ! 숫자 2개 입력
-# x, y = map(int, stdin.readline().strip().split())
+# x, y = map(int, input().split())
 
 # ! 숫자 여러 개 입력
-# num_list = list(map(int, stdin.readline().strip().split()))
+# num_list = list(map(int, input().split()))
 
 # ! 2차원 배열 입력
 # * 선언과 동시에 입력 받기
-# two_dimenion_array = [list(map(int, stdin.readline().strip().split())) for _ in range(y)]
+# two_dimenion_array = [list(map(int, input().split())) for _ in range(y)]
 
 # * 원소에 list 추가하면서 입력 받기
 # arr = []
 # for _ in range(y):
-#     arr.append(list(int, stdin.readline().strip().split()))
+#     arr.append(list(int, input().split()))
 
 # ! 2차원 배열 초기화
 # arr = [[0] * N for _ in range(N)]
 
 # 우선순위 큐
 # from queue import PriorityQueue
+# import heapq
+
+# q = []
+# heapq.heappush(q, iterm)
+# heapq.heappop(q)
